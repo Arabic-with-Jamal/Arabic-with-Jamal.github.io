0e83d63f14c8d4dd40d9c18ccc94eff21161716e
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-04-09 05:46:29 +0200

    Add screenshots

---
+++ Egyptian-Arabic/modules/lessons/lesson-005/lesson-005.js
@@ -0,0 +1,371 @@
+import { speaker1, speaker2, speaker4, narrator, audiobook, arabSpeaker4, arabSpeaker1} from "../lesson-001/lesson-001.js"
+
+export const lesson004 = {
+  chapter: 'Meeting and greeting',
+  title: 'My name is',
+  sections: [
+    {
+      title: 'Introduction',
+      slides: [
+        {
+          type: 'intro',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          title: 'Introduction',
+          intro: true,
+          description: `While they are stuck in traffic, ${speaker4} and ${speaker2} practice introductions with
+          ${speaker1}. When ${speaker2} stops at a gas station to fill up, ${speaker1} suddenly recognizes the cab that
+          drove away with his bag in it.`
+        },
+        {
+          type: 'intro',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          title: 'Introduction',
+          intro: true,
+          description: 'In this lesson you will learn:',
+          goals: [
+            '1- tp say your name',
+            "2- tsdsdsd",
+            '3- to ask for additional information and to ask people to repeat what they said ',
+          ],
+        }
+      ]
+    },
+    {
+      title: 'Story',
+      slides: [
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/002.png',
+          convo: `${speaker1} showing them the picture`,
+          speaker: narrator,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/003.png',
+          convo: 'دي يارا',
+          speaker: speaker1,
+          vocab: [
+            {word: 'دي', meaning: 'this (f)', url: '/audio1970771774_03.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/004.png',
+          convo: 'يارا مين؟',
+          speaker: speaker2,
+          vocab: [
+            {word: 'مين', meaning: 'who', url: '/audio1970771774_03.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/005.png',
+          convo: `${speaker1} trying hard to speak Arabic`,
+          speaker: narrator,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/006.png',
+          convo: 'خالتي',
+          speaker: speaker1,
+          vocab: [
+            {word: 'خالة', meaning: 'aunt (mother side)', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/007.png',
+          convo: 'خالتك؟',
+          speaker: speaker4,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/008.png',
+          convo: 'مستحيل',
+          speaker: speaker4,
+          vocab: [
+            {word: 'مستحيل', meaning: 'impossible', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/009.png',
+          convo: 'بس، مالناش دعوة',
+          speaker: speaker2,
+          vocab: [
+            {word: 'بس', meaning: 'enough, stop', url: '/audio1970771774_01.mp3'},
+            {word: 'مالناش دعوة', meaning: 'not our bussines', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/010.png',
+          convo: 'انت عايز تروح لخالتك؟',
+          speaker: speaker2,
+          vocab: [
+            {word: 'عايز', meaning: 'want', url: '/audio1970771774_01.mp3'},
+            {word: 'تروح', meaning: 'to go', url: '/audio1970771774_01.mp3'},
+            {word: 'ل', meaning: 'to', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/011.png',
+          convo: 'العنوان معاك؟',
+          speaker: speaker2,
+          vocab: [
+            {word: 'عنوان', meaning: 'adress', url: '/audio1970771774_01.mp3'},
+            {word: 'معاك', meaning: 'with you', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/012.png',
+          convo: 'انت فاهم؟',
+          speaker: speaker2,
+          vocab: [
+            {word: 'فاهم', meaning: 'understand', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/013.png',
+          convo: 'انا مش فاهم',
+          speaker: speaker1,
+          vocab: [
+            {word: 'مش', meaning: 'not', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/014.png',
+          convo: 'متيجي نساعده',
+          speaker: speaker4,
+          vocab: [
+            {word: 'متيجي', meaning: "why don't we", url: '/audio1970771774_01.mp3'},
+            {word: 'نساعد', meaning: '{we} help', url: '/audio1970771774_01.mp3'},
+            {word: 'ه', meaning: 'him', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/015.png',
+          convo: 'المفروض نعمل كدة',
+          speaker: speaker2,
+          vocab: [
+            {word: 'المفروض', meaning: 'supposed to', url: '/audio1970771774_01.mp3'},
+            {word: 'نعمل', meaning: 'do', url: '/audio1970771774_01.mp3'},
+            {word: 'كدة', meaning: 'like this', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/016.png',
+          convo: 'ماشي تعالي نشوف ممكن نعمل ايه',
+          speaker: speaker2,
+          vocab: [
+            {word: 'ماشي', meaning: 'ok', url: '/audio1970771774_01.mp3'},
+            {word: 'تعالي', meaning: "let's", url: '/audio1970771774_01.mp3'},
+            {word: 'نشوف', meaning: 'see', url: '/audio1970771774_01.mp3'},
+            {word: 'ممكن', meaning: 'possible', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/017.png',
+          convo: 'تعالى ناخدك معانا',
+          speaker: speaker2,
+          vocab: [
+            {word: 'معانا', meaning: 'with us', url: '/audio1970771774_01.mp3'},
+            {word: 'ناخد', meaning: 'we take', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/018.png',
+          convo: 'يلا بينا',
+          speaker: speaker4,
+          vocab: [
+            {word: 'يلا بينا', meaning: "let's go", url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/019.png',
+          convo: `انا اسمي ${arabSpeaker4}`,
+          speaker: speaker4,
+          vocab: [
+            {word: 'اسمي', meaning: 'my name is', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/020.png',
+          convo: `ودي عمتي`,
+          speaker: speaker4,
+          vocab: [
+            {word: 'و', meaning: 'and', url: '/audio1970771774_01.mp3'},
+            {word: 'عمة', meaning: 'aunt (father side)', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/021.png',
+          convo: 'امك؟',
+          speaker: speaker1,
+          vocab: [
+            {word: 'ام', meaning: 'mother', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/022.png',
+          convo: 'لا مش امي، عمتي',
+          speaker: speaker4,
+          vocab: [
+            {word: 'لا', meaning: 'no', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/023.png',
+          convo: 'خد شوية شيبسي',
+          speaker: speaker4,
+          vocab: [
+            {word: 'خد', meaning: 'take (informal)', url: '/audio1970771774_01.mp3'},
+            {word: 'شوية', meaning: 'some/little', url: '/audio1970771774_01.mp3'},
+            {word: 'شيبسي', meaning: 'chips', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/024.png',
+          convo: 'شكرا',
+          speaker: speaker1,
+          vocab: [
+            {word: 'شكرا', meaning: 'thanks', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/025.png',
+          convo: 'عفوا',
+          speaker: speaker4,
+          vocab: [
+            {word: 'عفوا', meaning: 'you are welcome', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/026.png',
+          convo: `انا اسمي ${arabSpeaker1}`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/027.png',
+          convo: `اهلا ${arabSpeaker1}`,
+          speaker: speaker4,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-005/images/028.png',
+          convo: `اهلا ${arabSpeaker4}`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+      ]
+    },
+    {
+      title: 'vocabulary excersises',
+      slides: [
+        {
+          type: 'section-intro',
+        },
+        {
+          type: 'vocabulary excersises',
+          title: 'flash cards',
+          quizletUrl: "https://quizlet.com/893648898/flashcards/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary excersises',
+          title: 'Learn',
+          quizletUrl: "https://quizlet.com/893648898/learn/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary excersises',
+          title: 'Spell',
+          quizletUrl: "https://quizlet.com/893648898/write/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary excersises',
+          title: 'test',
+          quizletUrl: "https://quizlet.com/893648898/test/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary excersises',
+          title: 'play',
+          quizletUrl: "https://quizlet.com/893648898/match/embed?i=2zi8pz&x=1jj1",
+        },
+      ]
+    },
+    {
+      title: 'Grammar',
+      slides: [
+        {
+          type: 'section-intro',
+          imgURl:'./modules/lessons/lesson-005/images/001.png'
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          description: `The possesive adjective (my) in Egyptian Arabic is achived by adding
+          letter ya (ي) at the end of the noun`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          description: `For example: Ism (اسم) = name, becomes: ismi (اسمي) = my name`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          description: `Umm (ام) = mother, becomes: ummi (امي) = my mom`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          description: `Can you guess what (my teacher) would be in Arabic?`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-005/images/001.png',
+          description: `You might want to be careful when shaking hands with the opposite sex.
+          Many people would not mind, but some would`
+        },
+      ]
+    },
+  ]
+}
