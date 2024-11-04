bc6d4a103e028e0df36962cdb1d384b8df393273
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-11-04 04:27:42 +0200

    Added a new lesson

--- Egyptian-Arabic/modules/lessons.js
+++ Egyptian-Arabic/modules/lessons.js
@@ -5,7 +5,8 @@ import { lesson004 } from "./lessons/lesson-004/lesson-004.js";
 import { lesson005 } from "./lessons/lesson-005/lesson-005.js";
 import { lesson006 } from "./lessons/lesson-006/lesson-006.js";
 import { lesson007 } from "./lessons/lesson-007/lesson-007.js";
+import { lesson008 } from "./lessons/lesson-008/lesson-008.js";
 
-const lessons = [lesson001, lesson002, lesson003, lesson004, lesson005, lesson006, lesson007]
+const lessons = [lesson001, lesson002, lesson003, lesson004, lesson005, lesson006, lesson007, lesson008]
 
 export default lessons
---
+++ Egyptian-Arabic/modules/lessons/lesson-008/lesson-008.js
@@ -0,0 +1,443 @@
+import { speaker1, speaker2, speaker4, narrator, audiobook, arabSpeaker4, arabSpeaker1, arabSpeaker2, max, arabMax} from "../lesson-001/lesson-001.js"
+
+export const lesson008 = {
+  chapter: 'Meeting people',
+  title: 'He has a problem',
+  sections: [
+    {
+      title: 'Introduction',
+      slides: [
+        {
+          type: 'intro',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          title: 'Introduction',
+          intro: true,
+          description: `${speaker2}, ${speaker1} and ${speaker4} are about to drive on when ${speaker2} runs into her friend 
+          ${max}. ${max} asks ${speaker1} who he is and where he comes from. When it becomes clear that ${speaker1} 
+          doesn’t know how to find his aunt, ${max} has an idea.`
+        },
+        {
+          type: 'intro',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          title: 'Introduction',
+          intro: true,
+          description: 'In this lesson you will learn:',
+          goals: [
+            '1- how to introduce yourself',
+            '2- how to speak about names, where you come from and where you live',
+          ],
+        }
+      ]
+    },
+    {
+      title: 'Story',
+      slides: [
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/002.png',
+          convo: `${arabSpeaker2}`,
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/003.png',
+          convo: `${arabMax} ازيك؟`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/004.png',
+          convo: 'زي الفل الحمد لله',
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/005.png',
+          convo: `عاملة ايه يا دنيا؟`,
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/006.png',
+          convo: `كويسة يا عمو`,
+          speaker: speaker4,
+          vocab: [
+            {word: 'عمو', meaning: 'a word that kids call adults', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/007.png',
+          convo: 'وحضرتك اسمك ايه؟',
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/008.png',
+          convo: `انا ${arabSpeaker1}`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/009.png',
+          convo: `اتشرفنا`,
+          speaker: max,
+          vocab: [
+            {word: 'اتشرفنا', meaning: 'we are honored', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/010.png',
+          convo: `انت مش من مصر صح؟`, 
+          speaker: max,
+          vocab: [
+            {word: 'صح', meaning: 'right', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/011.png',
+          convo: 'معلش انا مش فاهم',
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/012.png',
+          convo: 'يعني انت منين؟',
+          speaker: max,
+          vocab: [
+            {word: 'يعني', meaning: 'means', url: '/audio1970771774_01.mp3'},
+            {word: 'منين', meaning: 'where from', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/013.png',
+          convo: `انا ${arabMax} من مصر`,
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/014.png',
+          convo: 'وانا دنيا من مصر برضه',
+          speaker: speaker4,
+          vocab: [
+            {word: 'برضه', meaning: 'too', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/015.png',
+          convo: `انا ${arabSpeaker1} من امريكا`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/016.png',
+          convo: ` انت عايش فين في امريكا`,
+          speaker: max,
+          vocab: [
+            {word: 'عايش', meaning: 'live', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/017.png',
+          convo: `مش فاهم معلش`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/018.png',
+          convo: `يعني انت منين في امريكا`,
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/019.png',
+          convo: `من كاليفورنيا ولا نيو يورك`,
+          speaker: max,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/020.png',
+          convo: `انا من كوليرادو`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'مفيش جديد', meaning: 'no new words', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/021.png',
+          convo: `${arabSpeaker1} كان برضه في المطار`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'كان', meaning: 'was', url: '/audio1970771774_03.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/022.png',
+          convo: `بس هو عنده مشكلة`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'بس', meaning: 'but', url: '/audio1970771774_01.mp3'},
+            {word: 'عنده', meaning: 'he has', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/023.png',
+          convo: `شنطته ضايعة`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'ـه', meaning: 'his', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/024.png',
+          convo: 'وبعدين هتعمل ايه دلوقتي؟',
+          speaker: max,
+          vocab: [
+            {word: 'دلوقتي', meaning: 'now', url: '/audio1970771774_01.mp3'},
+            {word: 'هتعمل', meaning: 'you will do', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/025.png',
+          convo: `انا بدور على خالتي`,
+          speaker: speaker1,
+          vocab: [
+            {word: 'بدور على', meaning: 'I am looking for', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/026.png',
+          convo: `خالة ${arabSpeaker1} عايشة هنا في مصر`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'عايشة', meaning: 'live', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/027.png',
+          convo: `بس هو مش معاه العنوان`, 
+          speaker: speaker2,
+          vocab: [
+            {word: 'معاه', meaning: 'he has [on him now]', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/028.png',
+          convo: 'ممكن تتصل بيها؟',
+          speaker: max,
+          vocab: [
+            {word: 'تتصل بيها', meaning: 'you call her', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/029.png',
+          convo: 'الرقم كان في الشنطة',
+          speaker: speaker1,
+          vocab: [
+            {word: 'رقم', meaning: 'number', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/030.png',
+          convo: 'وتليفوني كمان كان في الشنطة',
+          speaker: speaker1,
+          vocab: [
+            {word: 'تليفون', meaning: 'phone', url: '/audio1970771774_01.mp3'},
+            {word: 'كمان', meaning: 'as well', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/031.png',
+          convo: 'مش انتي باباكي ظابط؟',
+          speaker: max,
+          vocab: [
+            {word: 'ظابط', meaning: 'officer', url: '/audio1970771774_01.mp3'},
+            {word: 'بابا', meaning: 'dad', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/032.png',
+          convo: `يعني ممكن يساعدنا`,
+          speaker: max,
+          vocab: [
+            {word: 'يساعد', meaning: 'he helps', url: '/audio1970771774_01.mp3'},
+            {word: 'نا', meaning: 'us', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/033.png',
+          convo: `فكرة حلوة`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'فكرة', meaning: 'idea', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/034.png',
+          convo: `ثانية واحدة`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'ثانية', meaning: 'second', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+        {
+          type: 'Story',
+          imgURl: './modules/lessons/lesson-008/images/035.png',
+          convo: `الو`,
+          speaker: speaker2,
+          vocab: [
+            {word: 'الو', meaning: 'phone greeting', url: '/audio1970771774_01.mp3'},
+          ]
+        },
+      ]
+    },
+    {
+      title: 'vocabulary exercises',
+      slides: [
+        {
+          type: 'section-intro',
+        },
+        {
+          type: 'vocabulary exercises',
+          title: 'flash cards',
+          quizletUrl: "https://quizlet.com/965852179/flashcards/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary exercises',
+          title: 'Learn',
+          quizletUrl: "https://quizlet.com/965852179/learn/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary exercises',
+          title: 'Spell',
+          quizletUrl: "https://quizlet.com/965852179/write/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary exercises',
+          title: 'test',
+          quizletUrl: "https://quizlet.com/965852179/test/embed?i=2zi8pz&x=1jj1",
+        },
+        {
+          type: 'vocabulary exercises',
+          title: 'play',
+          quizletUrl: "https://quizlet.com/965852179/match/embed?i=2zi8pz&x=1jj1",
+        },
+      ]
+    },
+    {
+      title: 'Grammar',
+      slides: [
+        {
+          type: 'section-intro',
+          imgURl:'./modules/lessons/lesson-008/images/001.png'
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `The present tense in Arabic is formed by using the infinitive form of the verb.`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `we only need to add the letter (ب) before the first letter 
+          (indicatory letter) to form the present tense.`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `This present letter (ب) should have a Kasra. except for the (I) form, which will be Fatha.`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `With the (I) form, the indicative letter (ا) will be dropped when we add the 
+          present letter (ب).`
+        },        
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `For example, he writes is بِيِكْتِبْ.`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `we write is بِنِكْتِبْ.`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `you write is بِتِكْتِبْ.`
+        },
+        {
+          type: 'grammar',
+          title: 'explanation',
+          imgURl: './modules/lessons/lesson-008/images/001.png',
+          description: `But I write is بَكْتِبْ. with the indicative letter (ا) dropped. and the 
+          present letter (ب) with a Fatha instead of a Kasra.`
+        },
+      ]
+    },
+  ]
+}
